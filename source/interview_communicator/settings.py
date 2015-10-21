e9abdc1313c67d0128e025c521f686770f8bb596
Ivaylo Bachvarof <bachvarof@gmail.com>
2015-10-21 11:28:20 +0300

    Add local_settings.py structure

--- source/interview_communicator/settings.py
+++ source/interview_communicator/settings.py
@@ -6,15 +6,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '14u_t_2p+*4r6jyj(!zg48h1ifbqga+q0szndh4rx-ce47yhb3'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -60,18 +51,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'interview_communicator.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
@@ -114,8 +93,3 @@ SUIT_CONFIG = {
     # misc
     'LIST_PER_PAGE': 100
 }
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
