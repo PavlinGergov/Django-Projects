f0d66cf9f99426bff72897b9ca3ae449bcaa3a48
Pavlin Gergov <gergov.pavlin@gmail.com>
2015-10-26 13:41:23 +0200

    Fix the api urs paths

--- source/course_interviews/urls.py
+++ source/course_interviews/urls.py
@@ -4,6 +4,6 @@ from .views import index, get_students, get_emails
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^api/get_students/(?P<course>[-\w]+)/$', get_students, name="get_students"),
-    url(r'^api/get_emails/$', get_emails, name="get_emails"),
+    url(r'^api/get-students/(?P<course>[-\w]+)/$', get_students, name="get_students"),
+    url(r'^api/get-emails/$', get_emails, name="get_emails"),
 ]
