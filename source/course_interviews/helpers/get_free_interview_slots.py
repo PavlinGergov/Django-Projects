13012bf670af07ec20c685ed3c914ffac1823d3f
Pavlin Gergov <gergov.pavlin@gmail.com>
2015-11-02 13:05:52 +0200

    Add buffer days for interviews

--- source/course_interviews/helpers/get_free_interview_slots.py
+++ source/course_interviews/helpers/get_free_interview_slots.py
@@ -6,18 +6,23 @@ def get_free_interview_slots():
     # Buffer slots are provided specially for the
     # students that can not attend their initial interview date
 
-    # buffer_slots = InterviewSlot.objects.filter(???)  # use proper filter
-    buffer_slots = []  # This variable is for testing purpose only!
+    # Buffer days
+    friday = datetime(2015, 11, 6)
+    saturday = datetime(2015, 11, 7)
 
-    # The other slots are the ones that do not have students assigned to them
-    other_slots = InterviewSlot.objects.all().order_by('teacher_time_slot__date')
+    buffer_slots = list(InterviewSlot.objects.all().filter(
+        teacher_time_slot__date=datetime.date(friday)).order_by('teacher_time_slot__date'))
+    buffer_slots += list(InterviewSlot.objects.all().filter(
+        teacher_time_slot__date=datetime.date(saturday)).order_by('teacher_time_slot__date'))
 
-    if len(buffer_slots) != 0:
-        available_slots = [slot for slot
-                           in buffer_slots
-                           if not slot.student]
+    available_slots = [slot for slot
+                       in buffer_slots
+                       if not slot.student]
+
     # If no bufers are left, use other available empty slots after the current day
-    else:
+    if len(available_slots) == 0:
+        # The other slots are the ones that do not have students assigned to them
+        other_slots = InterviewSlot.objects.all().order_by('teacher_time_slot__date')
         today = datetime.now()
         available_slots = [slot for slot
                            in other_slots
